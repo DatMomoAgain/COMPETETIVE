6250fe62b08e1bc747650983b9c48547b14e4385
Mohak Khetan <92690177+DatMomoAgain@users.noreply.github.com>
2022-12-05 04:00:29 +0530

    Create Maximum Depth of Binary Tree.cpp

---
+++ leet/data structure I/Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int* i = new int();
+        *i = 0;
+        int* max = new int();
+        *max=0;
+        traverse(root,*i, *max);
+        return *max;
+    }
+    
+    void traverse(TreeNode* root, int &i, int &max)
+    {
+        if(root)
+        {
+            i++;
+            if(i>max)
+                max = i;
+            cout<<"(i)"<<i<<endl;
+            traverse(root->left, i, max);
+            if (root->right!=NULL)
+                i--;
+            cout<<"(ii)"<<i<<endl;
+            if(root->right!=NULL)
+            {
+                i++;
+                cout<<"(iii)"<<i<<endl;
+            }
+            traverse(root->right, i, max);
+            i--;
+        }
+        
+    }
+};
